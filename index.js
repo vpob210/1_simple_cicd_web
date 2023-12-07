a0e2a6d73b63189eddd02bcc481a1f0418a07d16
vpob210 <146315049+vpob210@users.noreply.github.com>
2023-12-07 10:29:48 +0500

    Update index.js
    jenkins test#2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! kenkins test');
+  res.send('Hello, World! jenkins test');
 });
 
 app.listen(port, () => {
