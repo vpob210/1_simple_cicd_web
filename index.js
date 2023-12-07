a3996c44856364f9ec3807189e121c1f7ac3d075
vpob210 <146315049+vpob210@users.noreply.github.com>
2023-12-07 13:03:56 +0500

    Update index.js
    kenkins test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! jenkins test CICD non compile check');
+  res.send('Hello, World! jenkins test CICD non compile check 1');
 });
 
 app.listen(port, () => {
